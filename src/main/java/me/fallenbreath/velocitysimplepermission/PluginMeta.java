e65b08626d99113f3bea75f885f724647083eda2
Fallen_Breath <yqx791125833@gmail.com>
2023-10-29 19:40:59 +0800

    v0.2.1

--- src/main/java/me/fallenbreath/velocitysimplepermission/PluginMeta.java
+++ src/main/java/me/fallenbreath/velocitysimplepermission/PluginMeta.java
@@ -4,5 +4,5 @@ public class PluginMeta
 {
 	public static final String ID = "velocitysimplepermission";
 	public static final String NAME = "VelocitySimplePermission";
-	public static final String VERSION = "0.2.0";
+	public static final String VERSION = "0.2.1";
 }
